1cbce8e8f6646483f454e067db45481e83408726
Yashwant Samal <yashwantsamal6@gmail.com>
2021-03-09 16:43:20 +0530

    Series code

--- series/main.swift
+++ series/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+print("enter the number for which you want series")
+let n = Int(readLine()!)!
+var f=0
+var s=1
+var sum=0
+print("\(f) \(s)")
+for i in 3...n
+{
+    sum=f+s
+    print(" \(sum)")
+    f=s
+    s=sum
+}
 
